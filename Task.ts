f32c431a0ccc58805bd25603e22cffc813241ef6
bossammorn <galac3a@gmail.com>
2024-10-21 12:23:42 +0700

    Create Task.ts

---
+++ Task.ts
@@ -0,0 +1,25 @@
+export class Task{
+    title: string;
+    description: string;
+    completed: boolean;
+    private static taskCount: number = 0;
+
+    constructor(title: string, description: string){
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+        Task.taskCount++;
+    }
+
+    markCompleted(): void {
+        this.completed = true;
+    }
+
+    updateDescription(newDescription: string): void{
+        this.description = newDescription;
+    }
+
+    static totalTasks(): number{
+        return Task.taskCount;
+    }
+}
